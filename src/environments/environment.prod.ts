f94fc924da68f9dff782d43d242dc83a523b7343
Mohnish K <mohnish@ekmatra.store>
2022-07-14 12:15:39 +0100

    add prodcution env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+
 export const environment = {
-  production: true
+  production: false,
+  WEBURL: 'http://18.170.237.209:3025/',
+  JsonURL: 'https://jsonplaceholder.typicode.com/',
+  Newsapi:' https://newsapi.org/v2/top-headlines?country=in&apiKey=db9b6a7bd1de49f0a7dd8748c0d76d70',
+  busines:' https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=db9b6a7bd1de49f0a7dd8748c0d76d70'
 };
