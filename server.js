e8bfb49b80645915d315c31e6670dc9169eee580
Mohnish K <mohnish@ekmatra.store>
2022-07-14 13:59:00 +0100

    deploying heroku-app

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-// app.use(express.static('./dist/angular-app-heroku'));
-
-app.use(express.static('./dist/test-app'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/test-app-heroku/'}),
-);
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist/test-app'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/test-app/index.html'));});
 app.listen(process.env.PORT || 8080);
